4090c684cdbe0a35f7d5bce658ffe850e71bd02f
Mauricio Nehuen Velazquez <mauri_i9070@hotmail.com>
2023-07-01 19:27:24 -0300

    Se agregaron las validaciones para el form, una alerta por inputs vacios y la funcion cotizarCripto funciona correctamente

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-	import { onMounted, ref } from 'vue';
+	import { onMounted, ref, reactive } from 'vue';
+	import Alerta from './components/Alerta.vue';
 
 	const monedas = ref([
 		{ codigo: 'USD', texto: 'Dolar de Estados Unidos' },
@@ -9,6 +10,12 @@
 	]);
 
 	const criptomonedas = ref([]);
+	const error = ref('');
+
+	const cotizar = reactive({
+		moneda: '',
+		criptomoneda: '',
+	});
 
 	onMounted(() => {
 		const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
@@ -18,6 +25,17 @@
 				criptomonedas.value = data.Data;
 			});
 	});
+
+	const cortizarCripto = () => {
+		// Validar que cotizar este lleno
+		if (Object.values(cotizar).includes('')) {
+			error.value = 'Todos los campos son obligatorio';
+			return;
+		}
+
+		error.value = '';
+		console.log('Paso las validaciones');
+	};
 </script>
 
 <template>
@@ -25,10 +43,18 @@
 		<h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
 
 		<div class="contenido">
-			<form class="formulario">
+			<Alerta v-if="error">{{ error }}</Alerta>
+
+			<form
+				class="formulario"
+				@submit.prevent="cortizarCripto"
+			>
 				<div class="campo">
 					<label for="moneda">Moneda:</label>
-					<select id="moneda">
+					<select
+						id="moneda"
+						v-model="cotizar.moneda"
+					>
 						<option value="">-- Selecciona --</option>
 						<option
 							v-for="moneda in monedas"
@@ -41,7 +67,10 @@
 
 				<div class="campo">
 					<label for="cripto">Criptomoneda:</label>
-					<select id="cripto">
+					<select
+						id="cripto"
+						v-model="cotizar.criptomoneda"
+					>
 						<option value="">-- Selecciona --</option>
 						<option
 							v-for="criptomoneda in criptomonedas"
